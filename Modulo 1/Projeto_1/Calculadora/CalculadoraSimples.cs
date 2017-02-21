8d3c985ff58452ea1b62b834314b863db636b466
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-02-21 12:13:48 -0300

    Desenvolvimento do código de produção.

---
+++ Modulo 1/Projeto_1/Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+        /* Método para Adicionar 2 números */
+
+        public int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        /* Método para Multiplicar 2 números */
+
+        public int Multiplicar(int num1, int num2)
+        {
+            //Para fins de demo (bug) => return num1 * num2
+            return num1 + num2;
+        }
+    }
+}
