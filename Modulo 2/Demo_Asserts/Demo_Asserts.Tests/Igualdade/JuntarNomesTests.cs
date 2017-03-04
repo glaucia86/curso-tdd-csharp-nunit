d44fcf9527f1fd284b281734407235cb461937fb
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-03-04 00:00:00 -0300

    Início do Modulo 2 - Asserts - Strings

---
+++ Modulo 2/Demo_Asserts/Demo_Asserts.Tests/Igualdade/JuntarNomesTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace Demo_Asserts.Tests.Igualdade
+{
+    [TestFixture]
+    public class JuntarNomesTests
+    {
+        /* Método de teste - finalidade: Comparar duas strings iguais */
+        [Test]
+        public void DevoJuntarNomeCompleto()
+        {
+           var sut = new JuntarNomes();
+
+           var nomeCompleto = sut.Juntar("Glaucia", "Lemos");
+
+           Assert.That(nomeCompleto, Is.EqualTo("Glaucia Lemos"));
+        }
+
+
+        /* Método de teste - finalidade: Comparar strings em case sensitive */
+        [Test]
+        public void DevoJuntarNomeCompleto_CaseSensitive()
+        {
+            var sut = new JuntarNomes();
+
+            var nomeCompleto = sut.Juntar("glaucia", "lemos");
+
+            Assert.That(nomeCompleto, Is.EqualTo("GLAUCIA LEMOS").IgnoreCase);
+        }
+
+        /* Método de teste - finalidade: Comparar strings diferentes */
+        [Test]
+        public void DevoJuntarNomeCompleto_NaoIgual()
+        {
+            var sut = new JuntarNomes();
+
+            var nomeCompleto = sut.Juntar("Glaucia", "Lemos");
+
+            Assert.That(nomeCompleto, Is.Not.EqualTo("Jose Lemos"));
+        }
+    }
+}
---
+++ Modulo 2/Demo_Asserts/Demo_Asserts/JuntarNomes.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo_Asserts
+{
+    public class JuntarNomes
+    {
+        /* Método principal correspondente a Juntar 
+         * o Nome + Sobrenome */
+        public string Juntar(string nome, string sobrenome)
+        {
+            return nome + " " + sobrenome;
+            //Saída: Glaucia Lemos
+        }
+    }
+}
