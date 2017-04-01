c0d3dc9e0fe26f210e9645695b4ab2e1c9dbb3b9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-04-01 01:19:34 -0300

    Demo - Intervalos.

---
+++ Modulo 2/Demo_Asserts/Demo_Asserts.Tests/Intervalos/CaracteristicasJogadorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Demo_Asserts.Tests.Intervalos
+{
+    [TestFixture]
+    public class CaracteristicasJogadorTests
+    {
+        /* Método de teste responsável por testar se o jogador está ganhando 
+         * vida depois que dormiu */
+        [Test]
+        public void DevoAumentarVidaJogadorDepoisDormir()
+        {
+            // vida inicial do jogador = 100%
+            var sut = new CaracteristicasJogador { Vida = 100 };
+
+            sut.Dormir();
+
+            Assert.That(sut.Vida, Is.GreaterThan(100));
+        }
+
+        /* Método de teste responsável por testar se o jogador está ganhando 
+         * vida depois que dormiu, porém usando intervalos/faixas de valores */
+        [Test]
+        public void DevoAumentarVidaJogadorDepoisDormir_Intervalo()
+        {
+            var sut = new CaracteristicasJogador { Vida = 100 };
+
+            sut.Dormir();
+
+            Assert.That(sut.Vida, Is.InRange(101, 200));
+        }
+
+        /* 
+         *  1) Is.GreaterThanOrEqualTo() <= 100
+            2) Is.LessThan() > 100
+         *  3) Is.LessThanOrEqualTo() = > 100
+         *  
+         */
+    }
+}
---
+++ Modulo 2/Demo_Asserts/Demo_Asserts/CaracteristicasJogador.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_Asserts
+{
+    public class CaracteristicasJogador
+    {
+        /* Atributos do Jogador */
+        public int Vida { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Apelido { get; set; }
+
+        public bool NovoJogador { get; set; }
+
+        public List<string> Armas { get; set; }
+
+        /* Classe Construtor */
+        public CaracteristicasJogador()
+        {
+            Nome = GerarNome();
+
+            NovoJogador = true;
+
+            CriarArmasIniciais();
+        }
+
+        /* Método responsável por aumentar a vida do jogador */
+        public void Dormir()
+        {
+            var random = new Random();
+
+            // aumentar vida do jogador: 20/45/100
+            var aumentarVida = random.Next(1, 101);
+
+            Vida += aumentarVida;
+        }
+
+        /* Método responsável por tirar vida do jogador */
+        public void PerderVida(int perderVida)
+        {
+            Vida = Math.Max(1, Vida -= perderVida);
+        }
+
+        /* Método responsável por gerar nomes dos jogadores de maneira randômica */
+        public string GerarNome()
+        {
+            var nomes = new[]
+            {
+                "Spider-Man",
+                "Homem de Ferro",
+                "Capitão América",
+                "Homem Formiga",
+                "Hulk"
+            };
+
+            return nomes[new Random().Next(0, nomes.Length)];
+        }
+
+        /* Método responsável por listar armas disponíveis para o Jogador */
+        private void CriarArmasIniciais()
+        {
+            Armas = new List<string>()
+            {
+                "Espada Grande",
+                "Espada Curta",
+                "Escudo",
+                "Arco"
+            };
+        }
+    }
+}
