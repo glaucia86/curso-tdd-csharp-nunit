0f3c6318ace0fa38fdcef16caa0ca9275eefe447
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-05-21 23:26:08 -0300

    Alteração no arquivo 'CalculadoraTests.cs'

--- Modulo 1/Projeto_1/Calculadora.Tests/CalculadoraTests.cs
+++ Modulo 1/Projeto_1/Calculadora.Tests/CalculadoraTests.cs
@@ -2,59 +2,55 @@
 
 namespace Calculadora.Tests
 {
-    public class CalculadoraTests
+    /* Teste Classe Adicionar 2 Números */
+    [TestFixture]
+    public class CalculadoraSimplesTests
     {
-        /* Teste Classe Adicionar 2 Números */
-        [TestFixture]
-        public class CalculadoraSimplesTests
+        [Test]
+        public void DeveAdicionarDoisNumeros()
         {
-            [Test]
-            public void DeveAdicionarDoisNumeros()
-            {
-                //SUT
-                var sut = new CalculadoraSimples();
+            //SUT
+            var sut = new CalculadoraSimples();
 
-                var resultado = sut.Adicionar(5, 5);
+            var resultado = sut.Adicionar(5, 5);
 
-                Assert.That(resultado, Is.EqualTo(10));
-            }
+            Assert.That(resultado, Is.EqualTo(10));
+        }
 
-            /* Teste Classe Multiplicar 2 Números */
-            [Test]
-            public void DeveMultiplicarDoisNumeros()
-            {
-                //SUT
-                var sut = new CalculadoraSimples();
+        /* Teste Classe Multiplicar 2 Números */
+        [Test]
+        public void DeveMultiplicarDoisNumeros()
+        {
+            //SUT
+            var sut = new CalculadoraSimples();
 
-                var resultado = sut.Multiplicar(5, 3);
+            var resultado = sut.Multiplicar(5, 3);
 
-                Assert.That(resultado, Is.EqualTo(15));
-            }
+            Assert.That(resultado, Is.EqualTo(15));
+        }
 
-            /* Teste Classe Dividir 2 Números */
-            [Test]
-            public void DeveDividirDoisNumeros()
-            {
-                //SUT
-                var sut = new CalculadoraSimples();
+        /* Teste Classe Dividir 2 Números */
+        [Test]
+        public void DeveDividirDoisNumeros()
+        {
+            //SUT
+            var sut = new CalculadoraSimples();
 
-                var resultado = sut.Dividir(25, 5);
+            var resultado = sut.Dividir(25, 5);
 
-                Assert.That(resultado, Is.EqualTo(5));
-            }
+            Assert.That(resultado, Is.EqualTo(5));
+        }
 
-            /* Teste Classe Diminuir 2 Números */
-            [Test]
-            public void DeveDiminuirDoisNumeros()
-            {
-                //SUT
-                var sut = new CalculadoraSimples();
+        /* Teste Classe Diminuir 2 Números */
+        [Test]
+        public void DeveDiminuirDoisNumeros()
+        {
+            //SUT
+            var sut = new CalculadoraSimples();
 
-                var resultado = sut.Diminuir(30, 5);
+            var resultado = sut.Diminuir(30, 5);
 
-                Assert.That(resultado, Is.EqualTo(25));
-            }
-
+            Assert.That(resultado, Is.EqualTo(25));
         }
 
     }
